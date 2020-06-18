a9aa19d1b9fadb77834f96fcf3ee1aeae2f74b56
Vladimir <vaksakalovbot@yandex.ru>
2020-06-18 13:29:13 +0300

    Final commit

--- Lesson9HW1/DataManager.swift
+++ Lesson9HW1/DataManager.swift
@@ -9,13 +9,67 @@
 import Foundation
 
 class DataManager {
+    
     static let shared = DataManager()
-    
-    var amimationPresets = [String]()
-    var animationCurves = [String]()
-    
-    private init() {
         
-    }
+    let amimationPresets = ["slideLeft",
+                            "slideRight",
+                            "slideDown",
+                            "slideUp",
+                            "squeezeLeft",
+                            "squeezeRight",
+                            "squeezeDown",
+                            "squeezeUp",
+                            "fadeIn",
+                            "fadeOut",
+                            "fadeOutIn",
+                            "fadeInLeft",
+                            "fadeInRight",
+                            "fadeInDown",
+                            "fadeInUp",
+                            "zoomIn",
+                            "zoomOut",
+                            "fall",
+                            "shake",
+                            "pop",
+                            "flipX",
+                            "flipY",
+                            "morph",
+                            "squeeze",
+                            "flash",
+                            "wobble",
+                            "swing"]
+    
+    let animationCurves = ["easeIn",
+                           "easeOut",
+                           "easeInOut",
+                           "linear",
+                           "spring",
+                           "easeInSine",
+                           "easeOutSine",
+                           "easeInOutSine",
+                           "easeInQuad",
+                           "easeOutQuad",
+                           "easeInOutQuad",
+                           "easeInCubic",
+                           "easeOutCubic",
+                           "easeInOutCubic",
+                           "easeInQuart",
+                           "easeOutQuart",
+                           "easeInOutQuart",
+                           "easeInQuint",
+                           "easeOutQuint",
+                           "easeInOutQuint",
+                           "easeInExpo",
+                           "easeOutExpo",
+                           "easeInOutExpo",
+                           "easeInCirc",
+                           "easeOutCirc",
+                           "easeInOutCirc",
+                           "easeInBack",
+                           "easeOutBack",
+                           "easeInOutBack"]
+    
+    private init() {}
     
 }
--- Lesson9HW1/ViewController.swift
+++ Lesson9HW1/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import Spring
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var animationSpringButton: SpringButton!
+    @IBOutlet var animationSpringView: SpringView!
+    @IBOutlet var presetNameLabel: UILabel!
+    @IBOutlet var curveNameLabel: UILabel!
+    
+    var curIndexPreset = 0
+    var nextIndexPreset = 0
+    var curIndexCurve = -1
+    var nextIndexCurve = 0
+    
+    let animationPresets = DataManager.shared.amimationPresets
+    let animationCurves = DataManager.shared.animationCurves
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationSpringView.layer.cornerRadius = 20
+        animationSpringButton.layer.cornerRadius = 10
+        animationSpringButton.titleLabel?.numberOfLines = 2
+        animationSpringButton.setTitle("Next preset: \(animationPresets[nextIndexPreset])\nNext curve: \(animationCurves[nextIndexCurve])",
+            for: .normal)
+        presetNameLabel.text = ""
+        curveNameLabel.text = ""
     }
 
-
+    @IBAction func aminateButtonTapped() {
+        curIndexCurve += 1
+        if curIndexCurve == animationCurves.count {
+            curIndexCurve = 0
+            curIndexPreset += 1
+            if curIndexPreset == animationPresets.count {
+                curIndexPreset = 0
+            }
+        }
+        
+        nextIndexCurve = curIndexCurve + 1
+        if nextIndexCurve == animationCurves.count {
+            nextIndexCurve = 0
+            nextIndexPreset = curIndexPreset + 1
+            if nextIndexPreset == animationPresets.count {
+                nextIndexPreset = 0
+            }
+        }
+        
+        animationSpringButton.setTitle("Next preset: \(animationPresets[nextIndexPreset])\nNext curve: \(animationCurves[nextIndexCurve])",
+            for: .normal)
+        
+        showAnimation(animationPresetName: animationPresets[curIndexPreset],
+                      animationCurveName: animationCurves[curIndexCurve])
+        
+        animationSpringButton.animation = "wobble"
+        animationSpringButton.animate()
+    }
+    
+    private func showAnimation(animationPresetName: String, animationCurveName: String) {
+        presetNameLabel.text = animationPresetName
+        curveNameLabel.text = animationCurveName
+        animationSpringView.animation = animationPresetName
+        animationSpringView.curve = animationCurveName
+        animationSpringView.animate()
+    }
+        
 }
 
